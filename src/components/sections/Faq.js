8ea2f27575d712ba8e1a3b639c27a8f603afa7e5
MetaConsultingGroup <77215416+MetaConsultingGroup@users.noreply.github.com>
2021-02-23 21:01:38 +0000

    Update Faq.js

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -1,4 +1,1 @@
 
-<div class="calendly-inline-widget" data-url="https://calendly.com/gritcult" style="min-width:320px;height:630px;"></div>
-<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-
